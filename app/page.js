b2ed797767f157b033f017950d3d3c46c530d2d6
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-08-09 07:55:28 +0100

    changed the first page view but will be looking at ensuring i get a better home page that connects both

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Movie from "@/components/movieFiles/Movie";
 
 export default function Home() {
 
   return ( 
-    <div>
-      page
+    <div className='md:px-12 py-4 px-5 flex flex-col gap-5'>
+
+      <h2 className='text-black font-semibold text-2xl'>Movies</h2>
+
+      <div className='sm:flex gap-12'>
+       {/*    <Sidebar /> */}
+        <Movie />
+      </div>
+      
     </div>
   );
 }
